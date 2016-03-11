f1240004bf92996c022a8d90ebb0f964482c1f1e
VioletLove <https://github.com/VioletLove/>
2016-03-11 20:16:05 +0800

    more.py 类似于Linux的more工具

---
+++ chp02/more.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+分割字符串或文本文件并交互进行分页
+"""
+
+
+def more(text, numberlines=15):
+    lines = text.splitlines()       # 效果类似spilt('\n')，不过不用在末尾加''
+    while lines:
+        chunk = lines[:numberlines]
+        lines = lines[numberlines:]
+        for line in chunk:
+            print(line)
+        if lines and input('''--More--[press key 'y' or 'Y' to continue]''') not in ['y', 'Y']:
+            break
+
+if __name__ == '__main__':
+    import sys
+    more(open(sys.argv[1]).read(), 10)
