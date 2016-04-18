f407eb249dd806360587d49b03a495a64cc53d64
VioletLove <https://github.com/VioletLove/>
2016-04-18 21:12:22 +0800

    遍历目录树

---
+++ python3Learning/chp04/lister_recur.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import sys, os
+
+def lister(curdir):
+    print('[' + curdir + ']' )
+    for file in os.listdir(curdir):
+        path = os.path.join(curdir, file)
+        if not os.path.isdir(path):
+            print(path)
+        else:
+            lister(path)
+
+if __name__ == '__main__':
+    lister(sys.argv[1])
