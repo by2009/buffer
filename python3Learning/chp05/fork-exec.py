b0cff17c8e2492ee6113fcc1e8aea514a192a335
VioletLove <https://github.com/VioletLove/>
2016-04-19 18:44:33 +0800

    fork和exec组合，运行真正的子进程(实际上是替换子进程内容)

---
+++ python3Learning/chp05/fork-exec.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import os
+
+parm = 0
+while True:
+    parm += 1
+    pid = os.fork()
+    if pid == 0:
+        os.execlp('python3', 'python3', 'child.py', str(parm))
+        assert False, 'error starting program'
+    else:
+        print('child is', pid)
+    if input() == 'q' : break
