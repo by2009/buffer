09e7cc631cea5a8752590671d5e173ff2c4eb7d9
VioletLove <https://github.com/VioletLove/>
2016-04-23 11:55:20 +0800

    可移植地合并文件

---
+++ python3Learning/chp06/join.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+
+#######################################################################
+合并split.py创建的目录下所有组分文件以重建文件。
+大概相当于Unix下的"cat fromdir/* > tofile"命令，不过可移植性和可配置性
+更好，并且将合并操作作为可以重复使用的函数而输出。依赖文件名的排序顺序：
+长度必须一致。可以进一步扩展分割/合并，弹出Tkinter文件选择器。
+"""
+
+import os, sys
+
+readsize = 1024
+
+def join(fromdir, tofile):
+    output = open(tofile, 'wb')
+    parts = os.listdir(fromdir)
+    parts.sort()
+    for filename in parts:
+        filepath = os.path.join(fromdir, filename)
+        fileobj = open(filepath, 'rb')
+        while True:
+            filebytes = fileobj.read(readsize)
+            if not filebytes: break
+            output.write(filebytes)
+        fileobj.close()
+    output.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == '-help':
+        print('Use: join.py [from-dir-name to-file-name]')
+    else:
+        if len(sys.argv) != 3:
+            interactive = True
+            fromdir = input('Directory containing part files? ')
+            tofile = input('Name of file to be recreated? ')
+        else:
+            interactive = False
+            fromdir, tofile = sys.argv[1:]
+        absfrom, absto = map(os.path.abspath, [fromdir, tofile])
+        print('Joining', absfrom, 'to make', absto)
+
+        try:
+            join(fromdir, tofile)
+        except:
+            print('Error joining files:')
+            print(sys.exc_info()[0], sys.exc_info()[1])
+        else:
+            print('Join complete: see', absto)
+        if interactive: input('Press Enter key')
+
+
+
